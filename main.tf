67f6db78adf690ffa97b4d302d97bbced65e430c
Taj Roshith <88622844+tajroshith@users.noreply.github.com>
2021-09-01 22:03:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "xxxxx"
-    key    = "terraform.tfstate"
+    key    = "global/terraform.tfstate"
     region = "us-east-1"
     access_key = "xxxxxx"
     secret_key = "xxxxxx"
